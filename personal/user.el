f5022b1fdd56380e65cb8e7617188a1eac782ee8
Jon Eisen <jon@joneisen.me>
2015-12-13 13:02:06 -0700

    update user.el

--- personal/user.el
+++ personal/user.el
@@ -1,1 +1,16 @@
+;;; user -- User bindings in prelude
+
+;;; Commentary:
+
+;;; Code:
 (setq prelude-theme 'solarized-dark)
+(setq-default indent-tabs-mode nil)
+(global-linum-mode 1)
+(setq tab-width 2)
+(setq js-indent-level 2)
+(setq js2-strict-missing-semi-warning nil)
+
+(global-set-key [f1] 'shell)
+
+(provide 'user)
+;;; user.el ends here
